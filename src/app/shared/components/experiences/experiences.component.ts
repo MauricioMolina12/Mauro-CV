cee1ad8e38ce05df35a59a9d8d91a8fecfc6319a
Mauricio Molina <molinamau37@gmail.com>
2025-04-12 11:29:39 -0500

    adding new experience

--- src/app/shared/components/experiences/experiences.component.ts
+++ src/app/shared/components/experiences/experiences.component.ts
@@ -6,11 +6,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [NgFor, NgIf],
   templateUrl: './experiences.component.html',
-  styleUrl: './experiences.component.scss'
+  styleUrl: './experiences.component.scss',
 })
 export class ExperiencesComponent {
-
   experiences = [
+    {
+      company: 'MercaDatos',
+      time: 'Actuality',
+      logo: 'assets/mercadatos.jpg',
+      positions: [
+        {
+          title: 'Systems Practitioner',
+          description: '',
+        },
+      ],
+    },
     {
       company: 'Edutin Academy',
       time: '10 Months',
@@ -18,7 +28,7 @@ export class ExperiencesComponent {
       positions: [
         {
           title: 'Frontend & Mobile Android Developer',
-          description: `I designed and developed modern applications using Angular, Figma, and Ionic, ensuring high performance and adaptability across multiple devices. I built native features for Android using Kotlin as the primary language. I efficiently integrated REST and GraphQL APIs to optimize real-time data exchange, enhance scalability, and meet business objectives. I managed version control with Git, collaborating with cross-functional teams in agile environments to ensure timely and high-quality deliveries. Additionally, I implemented accessibility and optimization strategies to align products with current standards and improve the end-user experience.`
+          description: `I designed and developed modern applications using Angular, Figma, and Ionic, ensuring high performance and adaptability across multiple devices. I built native features for Android using Kotlin as the primary language. I efficiently integrated REST and GraphQL APIs to optimize real-time data exchange, enhance scalability, and meet business objectives. I managed version control with Git, collaborating with cross-functional teams in agile environments to ensure timely and high-quality deliveries. Additionally, I implemented accessibility and optimization strategies to align products with current standards and improve the end-user experience.`,
         },
         {
           title: 'Teacher and course designer',
@@ -27,23 +37,23 @@ export class ExperiencesComponent {
             {
               title: 'Angular',
               image: 'assets/angular.png',
-              slug: 'https://edutin.com/curso-de-angular'
+              slug: 'https://edutin.com/curso-de-angular',
             },
             {
               title: 'CSS',
               image: 'assets/css.png',
-              slug: 'https://edutin.com/curso-de-css3'
+              slug: 'https://edutin.com/curso-de-css3',
             },
             {
               title: 'Ionic',
               image: 'assets/ionic.png',
-              slug: 'https://edutin.com/curso-de-ionic'
-            }
-          ]
-
-        }
-      ]
+              slug: 'https://edutin.com/curso-de-ionic',
+            },
+          ],
+        },
+      ],
     },
+
     {
       company: 'Freelancer',
       time: '2 years',
@@ -51,9 +61,9 @@ export class ExperiencesComponent {
       positions: [
         {
           title: 'Frontend & Mobile Android Developer',
-          description: `I am a versatile, freelance frontend developer with experience building web and mobile applications using cutting-edge technologies such as Angular, React, React Native, Firebase, GraphQL, JavaScript, Ionic, Flutter and Node.js. My skills allow me to offer high-quality, customized solutions for a variety of industries, including education, startups and the technology sector.With a strong focus on user experience and design, I create intuitive and visually engaging interfaces that align with clients' goals. I also use tools like Figma to bring concepts to life, ensuring that functionality and aesthetics go hand in hand. As a freelancer, I pride myself on my adaptability, effective communication and timely delivery. Whether you need a responsive web app, a dynamic mobile solution, or expert guidance for your project, I'm here to help you make your vision a reality. Let's collaborate!`
+          description: `I am a versatile, freelance frontend developer with experience building web and mobile applications using cutting-edge technologies such as Angular, React, React Native, Firebase, GraphQL, JavaScript, Ionic, Flutter and Node.js. My skills allow me to offer high-quality, customized solutions for a variety of industries, including education, startups and the technology sector.With a strong focus on user experience and design, I create intuitive and visually engaging interfaces that align with clients' goals. I also use tools like Figma to bring concepts to life, ensuring that functionality and aesthetics go hand in hand. As a freelancer, I pride myself on my adaptability, effective communication and timely delivery. Whether you need a responsive web app, a dynamic mobile solution, or expert guidance for your project, I'm here to help you make your vision a reality. Let's collaborate!`,
         },
-      ]
+      ],
     },
     {
       company: 'Designer UI/UX Freelancer',
@@ -62,10 +72,9 @@ export class ExperiencesComponent {
       positions: [
         {
           title: 'Designer',
-          description: `I am an experienced graphic designer with experience in creating attractive and effective visual solutions that communicate ideas and emotions in an impactful way. I master tools such as Adobe Photoshop, Illustrator and InDesign, which allows me to develop everything from visual identities to advertising pieces and digital content. My approach combines creativity, technique and attention to detail to transform concepts into designs that stand out. I specialize in the design of logos, flyers, banners and content for social networks, adapting to the specific needs of each project and client. Passionate about design and always learning, I am ready to help you bring your ideas to life and make your brand or project stand out. Let's connect and work together!"`
+          description: `I am an experienced graphic designer with experience in creating attractive and effective visual solutions that communicate ideas and emotions in an impactful way. I master tools such as Adobe Photoshop, Illustrator and InDesign, which allows me to develop everything from visual identities to advertising pieces and digital content. My approach combines creativity, technique and attention to detail to transform concepts into designs that stand out. I specialize in the design of logos, flyers, banners and content for social networks, adapting to the specific needs of each project and client. Passionate about design and always learning, I am ready to help you bring your ideas to life and make your brand or project stand out. Let's connect and work together!"`,
         },
-      ]
+      ],
     },
-  ]
-
+  ];
 }
